a337812599c58f72fff0f4a33913209619598b16
avinash1906 <prasadansu708@gmail.com>
2020-01-09 17:34:06 +0530

    New Class Added

---
+++ src/test/java/com/Vtiger/OpportunityModule/SampleTest.java
@@ -0,0 +1,12 @@
+package com.Vtiger.OpportunityModule;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void m1()
+	{
+		System.out.println("m1 running");
+	}
+}
